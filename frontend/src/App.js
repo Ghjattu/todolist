c33971c2b8685797dc6b018ce233816ea72b6fe1
ghjattu <icracked369@gmail.com>
2023-05-30 17:54:07 +0800

    ✨ feat: ui增加登陆页面

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import DisplayForm from './components/Forms';
+
+
+const App = () => {
+	return (
+		<div className="container">
+			<DisplayForm/>
+		</div>
+	);
+};
+
+export default App;
